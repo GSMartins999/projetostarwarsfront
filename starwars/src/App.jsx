18cebcafb137a382ccfd244590c78606e3738bf5
Giovanni <giovannisouza216@gmail.com>
2026-02-27 12:56:40 -0300

    ajustes

--- starwars/src/App.jsx
+++ starwars/src/App.jsx
@@ -2,12 +2,30 @@ import './App.css'
 import Card from './components/card/Card'
 import Header from './components/header/header'
 
+const personagens = [{
+  id: 1,
+  nome: "Luke",
+  genero: "Masculino",
+  nascimento: 1970,
+  altura: "1.72m",
+  url:"https://tse3.mm.bing.net/th/id/OIP.6xywxeNGmrCpXrZHnzTIJwHaMB?rs=1&pid=ImgDetMain&o=7&rm=3"
+}, {
+  id: 2,
+  nome: "R2-D2",
+  genero: "Masculino",
+  nascimento: 1970,
+  altura: "0,80m",
+  url: "https://wallpapercave.com/wp/wp13481304.jpg"
+}
+];
+
+
 function App() {
 
   return (
     <>
-     <Header/>
-     <Card/>
+      <Header />
+      <Card itens={personagens} />
     </>
   )
 }
--- starwars/src/components/card/Card.jsx
+++ starwars/src/components/card/Card.jsx
@@ -2,42 +2,46 @@ import styles from './Card.module.css'
 import person from "./../../assets/person.svg"
 import heart from "./../../assets/heart.svg"
 
-function titulo(botaoclicado){
-    if(botaoclicado == "Favoritos"){
+function titulo(botaoclicado) {
+    if (botaoclicado == "Favoritos") {
         return "FAVORITOS";
-    } else{
+    } else {
         return "PERSONAGENS"
     }
 }
 
-function descritivo(botaoclicado){
-    if(botaoclicado == "Favoritos"){
+function descritivo(botaoclicado) {
+    if (botaoclicado == "Favoritos") {
         return "Gerencie seus personagens favoritos";
-    } else{
+    } else {
         return "Explore a galáxia e favorite seus personagens"
     }
 }
 
-export default function Card(props) {
+
+export default function Card({ itens }) {
     return (
         <>
+            {console.log(itens)}
             <div className={styles.container}>
-                <h1 className={styles.titulo}>{titulo(props.botaoclicado)}</h1>
-                <p className={styles.descritivo}>{descritivo(props.botaoclicado)}</p>
-                 <div className={styles.containercard}>
-                    <div>
-                        <img className={styles.iconefav} src={heart} alt="favoritos" />
+                {/* <h1 className={styles.titulo}>{titulo(props.botaoclicado)}</h1> */}
+                {/* <p className={styles.descritivo}>{descritivo(props.botaoclicado)}</p> */}
+
+                {itens.map(item => (
+                    <div style={{"--url-img": `url(${item.url})`}} className={styles.containercard}  key={item.id}>
+                        <div>
+                            <img className={styles.iconefav} src={heart} alt="favoritos" />
+                        </div>
+                        <div className={styles.containerInfo}>
+                            <h4 className={styles.titulo}>{item.nome}</h4>
+                            <p className={styles.descritivo}>Nascimento: {item.nascimento}</p>
+                            <p className={styles.descritivo}>Gênero: {item.genero}</p>
+                            <p className={styles.descritivo}>Altura: {item.altura}</p>
+                        </div>
                     </div>
-                    <img className={styles.iconeperson} src={person} alt="iconepessoa" />
-                    <div className={styles.containerInfo}>
-                        <h4 className={styles.nomepersonagem}>Luke SkyWalker</h4>
-                        <p className={styles.infos}>Nascimento: 1970</p>
-                        <p className={styles.infos}>Gênero: Masculino</p>
-                        <p className={styles.infos}>Altura: 1.72m</p>
-                    </div>
-                </div>
+                ))}
+
             </div>
-    
         </>
     )
 }   
